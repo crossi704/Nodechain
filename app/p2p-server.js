f9789e7233d7a78f52206e7d06f5ba63bf29e3a7
Caio Rossi <rossicaio@gmail.com>
2019-07-29 22:53:48 +0200

    p2pserver class fixed and Syncchains added

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -9,14 +9,48 @@ class P2pServer {
         this.sockets = [];
     }
 
-    listen {
+    listen() {
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
-        console.log('Listening for peer-to-peer connections on: ${P2P_PORT}');
+
+        this.connectToPeers();
+
+        console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            // ws://localhost:5001
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        this.sendChain(socket);
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+
+            this.blockchain.replaceChain(data);
+        });
+    }
+
+    sendChain(socket) {
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    syncChains() {
+        this.sockets.forEach(socket => this.sendChain(socket));
     }
 }
+
+module.exports = P2pServer;
